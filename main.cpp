df49bbe2e695088b2174689073bda42c60932dff
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2023-10-12 14:38:42 -0700

    Added color header file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 #include <fstream>
 
@@ -17,15 +20,8 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0;
-
-            int ir = static_cast<int>(255.999 * r); // convert 0.0-1.0 to 0-255
-            int ig = static_cast<int>(255.999 * g); // 1*255.999 casted to an int will
-            int ib = static_cast<int>(255.999 * b); // become 255
-            
-            img_file << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(img_file, pixel_color);
         }
     }
     img_file.close();
