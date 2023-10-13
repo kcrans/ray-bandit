68192f10d12dd183cbefda5097bdb0a54b0cde56
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2023-10-13 12:25:22 -0700

    Added ray class

--- main.cpp
+++ main.cpp
@@ -18,12 +18,12 @@ int main() {
     img_file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        std::clog << "\rCurrent Scanline: " << j << '/' << image_height - 1 << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
             write_color(img_file, pixel_color);
         }
     }
     img_file.close();
-    std::clog << "\rDone.                 \n";
+    std::clog << "\rDone.                    \n";
 }
