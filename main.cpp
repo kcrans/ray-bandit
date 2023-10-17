195856c7a6d962745dee24c9c1b1ccfd48c6a6ba
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2023-10-17 15:20:10 -0700

    Finshed debug info

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 bool hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
@@ -15,7 +16,7 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -20), 0.5, r))
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
 	    return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
@@ -23,7 +24,7 @@ color ray_color(const ray& r) {
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
     // Image Specifications
     auto aspect_ratio = 16.0 / 9.0; // Ideal ratio
@@ -35,8 +36,8 @@ int main() {
     image_height = (image_height < 1) ? 1 : image_height; 
 
     // Camera
+    auto focal_length = 1;
 
-    auto focal_length = 1.0;
     // Viewport heights and widths are real-valued
     // The aspect ratio is based off the calulated pixel aspect ratio
     auto viewport_height = 2.0; // Arbitrary value
@@ -54,6 +55,23 @@ int main() {
     // Calculate the location of the upper left pixel
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Debug info
+    if (argc == 2) {
+        std::string arg = argv[1];
+        if (arg == "--info") {
+            std::cout << "Ideal aspect ratio: " << aspect_ratio << std::endl;
+            std::cout << "Pixel height and width: " << image_height << 'x' << image_width << std::endl;
+            std::cout << "Viewport height and width: " << viewport_height << 'x' << viewport_width << std::endl;
+            std::cout << "Camera center and focal length: " << camera_center << ',' << ' ' << focal_length << std::endl;
+            std::cout << "V_{u}: " << viewport_u << std::endl;
+            std::cout << "V_{v}: " << viewport_v << std::endl;
+            std::cout << "Delta u: " << pixel_delta_u << std::endl;
+            std::cout << "Delta v: " << pixel_delta_v << std::endl;
+            std::cout << "Upper left viewport: " << viewport_upper_left << std::endl;
+            std::cout << "1st pixel location: " << pixel00_loc << std::endl;
+        }
+    }
 
     // Rendering Code
 
