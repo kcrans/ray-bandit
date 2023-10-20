1deea47830c10ae0792872750bcd94897a7028d8
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2023-10-20 13:06:15 -0700

    Simplified intersection code + shaded wrt. normals

--- main.cpp
+++ main.cpp
@@ -6,18 +6,26 @@
 #include <fstream>
 #include <string>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2*dot(r.direction(), oc);
-	auto c = dot(oc, oc) - radius*radius;
-	auto discriminant = b*b - 4*a*c;
-	return (discriminant >= 0);
+	auto a = r.direction().length_squared();
+	auto half_b = dot(r.direction(), oc);
+	auto c = oc.length_squared() - radius*radius;
+	auto discriminant = half_b*half_b - a*c;
+	
+    if (discriminant < 0) {
+        return -1.0; // Only complex solutions
+    } else { // Return the nearest solution
+        return (-half_b - sqrt(discriminant)) / a;
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-	    return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = (r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
