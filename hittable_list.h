acd08149fb9b48607eedc72bf10c973e5138225d
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2023-10-21 09:53:27 -0700

    Added classes for abstract hittable objs

---
+++ hittable_list.h
@@ -0,0 +1,19 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class hittable_list : public hittable {
+    public:
+    std::vector<shared_ptr<>>
+
+}
+
+
+#endif
